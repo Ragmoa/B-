1f53cd5795e7f397a96a4869cf13c8a0c31a3a9a
Rylux <ryckaert.guillaume@neuf.fr>
2018-02-03 18:26:07 -0500

    check_collision 1

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -55,16 +55,16 @@ public class Joueur {
 	}
 
 
-	 public void placer_bateau(int x , int y){
-		 int[][] cases=new int[17][2];		 
+	 /*public void placer_bateau(int x , int y){
+		 int[][] cases=new int[17][2];
 		 int pos[] = {x,y};
 		 int k = 0;
 		 for(int i = 0; i < 5 ; i++) {
 			 if(i==0)
 			 {
-				 
+
 				 if((bateaux[i].get_taille()+x < 10 && bateaux[i].is_horizontal()) || (bateaux[i].get_taille()+y < 10 && !bateaux[i].is_horizontal() ))
-				 { 
+				 {
 					 bateaux[i].set_position(pos);
 					 for(int j = 0 ; j < bateaux[i].get_taille() ; j++) {
 						 cases[k][0]=bateaux[i].cases_ocupees()[j][0];
@@ -76,28 +76,47 @@ public class Joueur {
 				 if((bateaux[i].get_taille()+x < 10 && bateaux[i].is_horizontal()) || (bateaux[i].get_taille()+y < 10 && !bateaux[i].is_horizontal() ))
 					 if(bateaux[i].get)
 		 }
-		 
+
 	    }
 
 		 for(int i = 0; i < 5 ; i++);
-	    }
+	 */
 
-	    public Boat select_bateau(int x, int y) {//TODO: Déplacer dans Joueur_humain 
-	        int i=0, j=0; 
-	          for (i=0;i<5;i++){ 
-	            for (j=0;j<this.bateaux[i].taille;j++){ 
-	              if (x==this.bateaux[i].cases_ocupees()[j][0] && y==this.bateaux[i].cases_ocupees()[j][1]){ 
-	                return this.bateaux[i]; 
-	              } 
-	            } 
-	          } 
-	          return null; 
-	      } 
-	 
-	      public void bouger(Boat b, int x, int y){ 
-	        int[] npos=new int[2]; 
-	        npos={x,y}; 
-	        b.set_position(npos); 
-	        return ; 
-	      } 
+			public boolean check_collision (Boat b){
+				int i=0, j=0,k=0;
+				for (i=0;i<5;i++){
+					if (this.bateaux[i]!=null){
+						for (j=0;j<this.bateaux[i].get_taille();j++){
+							for (k=0,k<b.get_taille();k++){
+								if (b.cases_ocupees()[k][0]==this.bateaux[i].cases_ocupees[j][0] && b.cases_ocupees()[k][1]==this.bateaux[i].cases_ocupees[j][1]){
+									return true;
+								}
+							}
+						}
+					}
+				}
+				return false;
+			}
+
+			public check_collision (Boat b, int x, int y){
+
+			}
+	    public Boat select_bateau(int x, int y) {//TODO: Dï¿½placer dans Joueur_humain
+	        int i=0, j=0;
+	          for (i=0;i<5;i++){
+	            for (j=0;j<this.bateaux[i].taille;j++){
+	              if (x==this.bateaux[i].cases_ocupees()[j][0] && y==this.bateaux[i].cases_ocupees()[j][1]){
+	                return this.bateaux[i];
+	              }
+	            }
+	          }
+	          return null;
+	      }
+
+	      public void bouger(Boat b, int x, int y){
+	        int[] npos=new int[2];
+	        npos={x,y};
+	        b.set_position(npos);
+	        return ;
+	      }
 }
