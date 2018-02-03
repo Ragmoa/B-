6af3978ee57648535baa37c6751c8d7326d24211
MANZANILLAF <manzanilla.francis@gmail.com>
2018-02-04 00:09:09 +0100

    modifscassé

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -56,47 +56,48 @@ public class Joueur {
 
 
 	 public void placer_bateau(int x , int y){
+		 int[][] cases=new int[17][2];		 
+		 int pos[] = {x,y};
+		 int k = 0;
+		 for(int i = 0; i < 5 ; i++) {
+			 if(i==0)
+			 {
+				 
+				 if((bateaux[i].get_taille()+x < 10 && bateaux[i].is_horizontal()) || (bateaux[i].get_taille()+y < 10 && !bateaux[i].is_horizontal() ))
+				 { 
+					 bateaux[i].set_position(pos);
+					 for(int j = 0 ; j < bateaux[i].get_taille() ; j++) {
+						 cases[k][0]=bateaux[i].cases_ocupees()[j][0];
+						 cases[k]1]=bateaux[i].cases_ocupees()[j][1];
+					 }
+				 }
+			 }
+			 else
+				 if((bateaux[i].get_taille()+x < 10 && bateaux[i].is_horizontal()) || (bateaux[i].get_taille()+y < 10 && !bateaux[i].is_horizontal() ))
+					 if(bateaux[i].get)
+		 }
+		 
+	    }
 
 		 for(int i = 0; i < 5 ; i++);
 	    }
 
-	    public void update_boat(Boat bateau, PlateauJeu plate) {
-	    	int posi[][] = bateau.cases_ocupees();
-	    	for(int i = 0 ;  i < posi[0].length ; i++) {
-	    		for(int j = 0 ; j < posi[1].length ; j++) {
-	    				plate.getCase(posi[i][0], posi[j][1]).setTypeC(Content.boat);
-	    		}
-	    	}
-	    }
-
-	    public void tirer(PlateauJeu plate, int i, int j) {
-	    	if(plate.getCase(i,j).getTypeC()==Content.boat) {
-	    		plate.getCase(i, j).setTypeC(Content.boat_hit);
-	    		System.out.println("touchï¿½");
-	    	}
-	    	else if(plate.getCase(i, j).getTypeC()==Content.boat_hit) {
-	    		System.out.println("bateau dï¿½jï¿½ abimï¿½");
-	    	}
-	    	else
-	    		System.out.println("ratï¿½");
-	    }
-
-      public Boat select_bateau(int x, int y) {//TODO: DÃ©placer dans Joueur_humain
-        int i=0, j=0;
-          for (i=0;i<5;i++){
-            for (j=0;j<this.bateaux[i].taille;j++){
-              if (x==this.bateaux[i].cases_ocupees()[j][0] && y==this.bateaux[i].cases_ocupees()[j][1]){
-                return this.bateaux[i];
-              }
-            }
-          }
-          return null;
-      }
-
-	    public void bouger(Boat b, int x, int y){
-        int[] npos=new int[2];
-        npos={x,y};
-        b.set_position(npos);
-        return ;
-	    }
+	    public Boat select_bateau(int x, int y) {//TODO: Déplacer dans Joueur_humain 
+	        int i=0, j=0; 
+	          for (i=0;i<5;i++){ 
+	            for (j=0;j<this.bateaux[i].taille;j++){ 
+	              if (x==this.bateaux[i].cases_ocupees()[j][0] && y==this.bateaux[i].cases_ocupees()[j][1]){ 
+	                return this.bateaux[i]; 
+	              } 
+	            } 
+	          } 
+	          return null; 
+	      } 
+	 
+	      public void bouger(Boat b, int x, int y){ 
+	        int[] npos=new int[2]; 
+	        npos={x,y}; 
+	        b.set_position(npos); 
+	        return ; 
+	      } 
 }
