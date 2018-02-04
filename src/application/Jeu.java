1693878d3d7b83b70d26b3cc813c521baf1d4ac4
MANZANILLAF <manzanilla.francis@gmail.com>
2018-02-04 21:58:27 +0100

    plas

--- src/application/Jeu.java
+++ src/application/Jeu.java
@@ -121,6 +121,11 @@ public class Jeu{
 			if(playerSide==true){
 				panel.majPanel(colonne, ligne, Content.boat_range);
 				j_actuel.setBateauplace(j_actuel.placer_bateau(colonne, ligne, j_actuel.getBateauplace(), true));
+				 j_actuel.setCases_joueur(j_actuel.get_player_boat()); 
+			       
+			      for(int i=0;i<j_actuel.get_player_boat().length;i++) { 
+			        panel.majPanel(j_actuel.get_player_boat()[i][0], j_actuel.get_player_boat()[i][1], Content.boat); 
+			        } 
 				if(j_actuel.getBateauplace()==5) {
 					panel.resetPanel();
 					autrePanel.resetPanel();		
@@ -168,6 +173,9 @@ public class Jeu{
 			break;
 		case 3 : //attente avant de changer de joueur
 			if(playerSide==false) {
+				for(int i=0;i<j_actuel.get_player_boat().length;i++) { 
+			        autrePanel.majPanel(j_actuel.get_player_boat()[i][0], j_actuel.get_player_boat()[i][1], Content.boat); 
+			        }
 				//On replace les hit/miss pour la suite
 				int tableauDroite[][]=j_actuel.get_status();
 				for(int i=0; i<10; i++) {
