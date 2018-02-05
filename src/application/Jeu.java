db9f10da5224f457f16358e862e3155bd497b45a
Rylux <ryckaert.guillaume@neuf.fr>
2018-02-04 19:41:08 -0500

    merge

--- src/application/Jeu.java
+++ src/application/Jeu.java
@@ -206,7 +206,9 @@ public class Jeu{
 		        }
 				 for(int i=0;i<j_actuel.get_player_range().length;i++) { 
 			          //System.out.println(j_actuel.getCases_portee()[i][0] + " " + j_actuel.getCases_portee()[i][1]); 
+					 	if (j_actuel.get_player_range()[i]!=null) {
 			              panel.majPanel(j_actuel.get_player_range()[i][0], j_actuel.get_player_range()[i][1], Content.boat_range);  
+					 		}
 			              } 
 				int[][] caseTouchee;
 				for(int i=0; i<5; i++) {
